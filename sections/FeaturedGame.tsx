b10052ea8041c3ba90643250bb7fc6b7632bb384
renad33 <44338279+renad33@users.noreply.github.com>
2026-01-23 18:55:11 +0300

    Update FeaturedGame section

--- sections/FeaturedGame.tsx
+++ sections/FeaturedGame.tsx
@@ -121,7 +121,7 @@ export default function FeaturedGame() {
 
       {/* Sticky Console - Scrolls normally first, then sticks to bottom */}
       <div className="sticky bottom-0 flex justify-center z-50 pointer-events-none">
-        <div className="relative w-[90vw] md:w-[85vw] lg:w-[1000px] min-[1920px]:w-[85vw] min-[2560px]:w-[80vw]">
+        <div className="relative w-[90vw] md:w-[85vw] lg:w-[1000px] max-w-[1000px]">
           {/* Video masked to screen area */}
           <div
             className="absolute z-30 overflow-hidden rounded-[2px] md:rounded-sm"
